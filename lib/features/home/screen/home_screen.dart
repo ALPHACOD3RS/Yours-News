51dee9c9f2c02bbef8ced9cc94d57f75a3e377b6
Naol Ketema <naolketema55@gmail.com>
2024-08-30 21:55:51 +0300

    all screen done

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:iconsax_plus/iconsax_plus.dart';
 import 'package:yours_news/features/home/provider/news_provider.dart';
-import 'package:yours_news/features/home/widget/custom_card.dart';
+import 'package:yours_news/features/home/widget/news_card1.dart';
+import 'package:yours_news/features/home/widget/news_card2.dart';
 import 'package:yours_news/shared/constant/colors.dart';
 import 'package:yours_news/shared/widget/custom_appbar.dart';
 
@@ -18,8 +19,8 @@ class HomeScreen extends HookConsumerWidget {
           onTap: () {},
           child: Container(
             height: 50,
-            width: 50,
-            padding: const EdgeInsets.all(8.0),
+            width: 60,
+            // padding: const EdgeInsets.all(8.0),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
               shape: BoxShape.rectangle,
@@ -31,7 +32,7 @@ class HomeScreen extends HookConsumerWidget {
             child: const Icon(IconsaxPlusLinear.menu),
           ),
         ),
-        leadingPadding: 20,
+        leadingPadding: 10,
         actions: [
           Container(
             height: 50,
@@ -53,67 +54,87 @@ class HomeScreen extends HookConsumerWidget {
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.symmetric(
-          horizontal: 18.0,
+          horizontal: 16.0,
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
-            const Text(
-              "Welcome back, Tyler!",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
-                color: Colors.black,
-              ),
-            ),
-            const Text(
-              "Discover a world of news that matters to you",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w400,
-                color: AppColor.textColor,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  "Trending news",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
-                  ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              const Text(
+                "Welcome back, Tyler!",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
                 ),
-                GestureDetector(
-                  child: const Text(
-                    "See all",
+              ),
+              const Text(
+                "Discover a world of news that matters to you",
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w400,
+                  color: AppColor.textColor,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Trending news",
                     style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      color: AppColor.textColor,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 310,
-              width: 500,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: cardItems.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                      width: 300, child: CustomCard(item: cardItems[index]));
-                },
+                  GestureDetector(
+                    child: const Text(
+                      "See all",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: AppColor.textColor,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
+              SizedBox(
+                height: 310,
+                width: double.infinity,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: cardItems.length,
+                  itemBuilder: (context, index) {
+                    return SizedBox(
+                      width: 300,
+                      child: NewsCard1(item: cardItems[index]),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: 24.0),
+              const Text(
+                "Recommendation",
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20,
+                  color: AppColor.color1A1A1A,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Column(
+                children: List.generate(cardItems.length, (index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: NewsCard2(item: cardItems[index]),
+                  );
+                }),
+              ),
+            ],
+          ),
         ),
       ),
     );
